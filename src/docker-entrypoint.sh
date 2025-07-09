645d35870351c9d2e34fa15a0be91ebbf62d0ce7
Kai <kai@umami-creative.de>
2025-07-09 15:38:02 +0200

    ♻️ Refactor Dockerfile to change user context for improved security and maintainability

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-WAL_USER=${WAL_USER:-postgres}
-WAL_GROUP=${WAL_GROUP:-postgres}
-
 # Fix permissions in /wal-archive folder
 if [ -d "/wal-archive" ]; then
+  WAL_USER=${WAL_USER:-postgres}
+  WAL_GROUP=${WAL_GROUP:-postgres}
+  
   echo "Setting proper permissions on /wal-archive folder"
   chown -R $WAL_USER:$WAL_GROUP /wal-archive
   chmod -R 700 /wal-archive
@@ -13,8 +13,6 @@ if [ -d "/wal-archive" ]; then
 else
   echo "Warning: /wal-archive folder does not exist"
 fi
-
-chown -R $WAL_USER:$WAL_GROUP /app
 
 # Execute the command passed to docker run
 exec "$@"
